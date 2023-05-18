aa3765cd85926464c094769f06352cd58b27de31
Jesse Slim <jesse.j.slim@gmail.com>
2023-05-18 14:07:38 +1000

    esa driver v1

--- pylabframe/hardware/device.py
+++ pylabframe/hardware/device.py
@@ -40,10 +40,21 @@ class Device:
     def __init__(self, id, error_on_double_connect=True, **kw):
         if id in _connected_devices and error_on_double_connect:
             raise RuntimeError(f"Device {id} already connected")
+        self.metadata_registry = {}
 
     @classmethod
     def list_available(cls):
         return []
+
+    def collect_metadata(self):
+        metadata_collection = {}
+        for k, v in self.metadata_registry.items():
+            value = v()
+            if isinstance(value, SettingEnum):
+                value = value.name
+            metadata_collection[k] = value
+
+        return metadata_collection
 
 
 class VisaDevice(Device):
@@ -66,23 +77,28 @@ class VisaDevice(Device):
             response = response.strip()
         return response
 
-    def wait_until_done(self):
-        self.visa_instr.write("*OPC?")
+    def wait_until_done(self, visa_cmd=None):
+        if visa_cmd is not None:
+            cmd_string = f"{visa_cmd};*OPC?"
+        else:
+            cmd_string = "*OPC?"
+        self.visa_instr.write(cmd_string)
         is_done = False
         while not is_done:
             try:
-                self.visa_instr.read()
+                result_code = self.visa_instr.read()
                 is_done = True
             except pyvisa.VisaIOError as e:
                 if e.error_code != pyvisa.constants.StatusCode.error_timeout:
                     # re-raise anything other than a time-out
                     raise e
+        return result_code
 
     @classmethod
     def visa_property(cls, visa_cmd: str, read_only=False, read_conv=str, write_conv=str, rw_conv=None):
         if rw_conv is not None:
             read_conv = rw_conv
-            write_conv = write_conv
+            write_conv = rw_conv
             
         def visa_getter(self: VisaDevice):
             # doing this gives us access to object properties (eg channel id) that can be put in the command string
@@ -104,21 +120,29 @@ class VisaDevice(Device):
             visa_setter = None
 
         prop = property(visa_getter, visa_setter)
+
         return prop
 
     @classmethod
-    def visa_command(cls, visa_cmd):
+    def visa_command(cls, visa_cmd, wait_until_done=False):
         def visa_executer(self: VisaDevice, **kw):
             if hasattr(self, "query_params"):
                 kw.update(self.query_params)
 
             fmt_visa_cmd = visa_cmd.format(**kw)
-            return self.visa_instr.write(fmt_visa_cmd)
+            if wait_until_done:
+                return self.wait_until_done(fmt_visa_cmd)
+            else:
+                return self.visa_instr.write(fmt_visa_cmd)
         return visa_executer
 
 
 def str_conv(s):
     return s.replace('"', '')
+
+
+def intbool_conv(s):
+    return bool(int(s))
 
 
 class SettingEnum(Enum):
--- pylabframe/hardware/drivers.py
+++ pylabframe/hardware/drivers.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from . import device
 from enum import Enum
-from .device import str_conv, SettingEnum
+from .device import str_conv, SettingEnum, intbool_conv
 import pylabframe.data
 
 # helper definition
@@ -25,8 +25,8 @@ class TektronixScope(device.VisaDevice):
 
     # global scpi properties
     record_length = visa_property("horizontal:recordlength", rw_conv=int)
-    run_mode = visa_property("acquire:stopafter", rw_conv=RunModes)
-    run_state = visa_property("acquire:state", read_conv=bool, write_conv=int)
+    run_mode = visa_property("acquire:stopafter", read_conv=RunModes)
+    running = visa_property("acquire:state", read_conv=intbool_conv, write_conv=int)
     x_scale = visa_property("horizontal:scale", rw_conv=float)
 
     # waveform transfer properties
@@ -81,7 +81,7 @@ class TektronixScope(device.VisaDevice):
         y_scale = visa_property("ch{channel_id}:scale", rw_conv=float)
         offset = visa_property("ch{channel_id}:offset", rw_conv=float)
         termination = visa_property("ch{channel_id}:termination", rw_conv=float)
-        inverted = visa_property("ch{channel_id}:invert", read_conv=bool, write_conv=int)
+        inverted = visa_property("ch{channel_id}:invert", read_conv=intbool_conv, write_conv=int)
 
         mean = visa_property("measu:meas{channel_id}:mean", read_only=True, read_conv=float)
 
@@ -90,11 +90,6 @@ class TektronixScope(device.VisaDevice):
 
 
 class KeysightESA(device.VisaDevice):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.visa_instr.write_termination = '\n'
-        self.visa_instr.read_termination = '\n'
-
     class RunModes(SettingEnum):
         CONTINUOUS = "1"
         SINGLE = "0"
@@ -103,20 +98,125 @@ class KeysightESA(device.VisaDevice):
         SPECTRUM_ANALYZER = "SA"
         IQ_ANALYZER = "BASIC"
 
-    instrument_mode = visa_property("inst:sel", rw_conv=InstrumentModes)
-    run_mode = visa_property("initiate:continuous", rw_conv=RunModes)
+    # see page 1000 of the EXA SA manual to find what these modes mean
+    class DetectorModes(SettingEnum):
+        NORMAL = "NORM"
+        AVERAGE = "AVER"
+        POSITIVE_PEAK = "POS"
+        SAMPLE = "SAMP"
+        NEGATIVE_PEAK = "NEG"
+        QUASI_PEAK = "QPE"
+        EMI_AVERAGE = "EAV"
+        RMS_AVERAGE = "RA"
+
+    class YUnits(SettingEnum):
+        dBm = "DBM"
+        dBmV = "DBMV"
+        dBmA = "DBMA"
+        V = "V"
+        W = "W"
+        A = "A"
+        dBuV = "DBUV"
+        dBuA = "DBUA"
+        dBpW = "DBPW"
+        dBuVm = "DBUVM"
+        dBuAm = "DBUAM"
+        dBPT = "DBPT"
+        dBG = "DBG"
+
+    class ScaleType(SettingEnum):
+        LOG = "LOG"
+        LINEAR = "LIN"
+
+    class TraceAverageModes(SettingEnum):
+        RMS = "RMS"
+        LOG_POWER = "LOG"
+        VOLTAGE = "SCAL"
+
+    METADATA_FIELDS = [
+        "center_frequency",
+        "span",
+        "start_frequency",
+        "stop_frequency",
+        "rbw",
+        "vbw",
+        "detector",
+        "sweep_time",
+        "record_length",
+        "trace_averaging",
+        "trace_average_count",
+        "trace_average_mode",
+        "y_unit"
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.visa_instr.write_termination = '\n'
+        self.visa_instr.read_termination = '\n'
+
+        for mf in self.METADATA_FIELDS:
+            self.metadata_registry[mf] = lambda self=self, mf=mf: getattr(self, mf)
+
+    instrument_mode = visa_property("inst:sel", read_conv=InstrumentModes)
+    run_mode = visa_property("initiate:continuous", read_conv=RunModes)
 
     center_frequency = visa_property("sense:freq:center", rw_conv=float)
     span = visa_property("sense:freq:span", rw_conv=float)
     start_frequency = visa_property("sense:freq:start", rw_conv=float)
     stop_frequency = visa_property("sense:freq:stop", rw_conv=float)
     rbw = visa_property("sense:band", rw_conv=float)
+    vbw = visa_property("sense:band:video", rw_conv=float)
+    auto_vbw = visa_property("sense:band:video:auto", read_conv=intbool_conv, write_conv=int)
+
+    detector = visa_property("sense:detector:trace", read_conv=DetectorModes)
 
     # acquisition_time = visa_property("sense:acquisition:time", rw_conv=float)
     # record_length = visa_property("sense:acquisition:points", rw_conv=int)
     sweep_time = visa_property("sense:sweep:time", rw_conv=float)
-    average_count = visa_property("sense:average:count", rw_conv=int)
+    record_length = visa_property("sense:sweep:points", rw_conv=int)
+    trace_average_count = visa_property("sense:average:count", rw_conv=int)
+    trace_averaging = visa_property("sense:average:state", read_conv=intbool_conv, write_conv=int)
+    trace_average_mode = visa_property("sense:average:type", read_conv=TraceAverageModes)
+    auto_trace_average_mode = visa_property("sense:average:type:auto", read_conv=intbool_conv, write_conv=int)
 
-    y_unit = visa_property("unit:power")
+    y_unit = visa_property("unit:power", read_conv=YUnits)
+    y_scale = visa_property("display:window:trace:y:spacing", read_conv=ScaleType)
 
     start_measurement = visa_command("initiate:immediate")
+    # start_measurement_and_wait = visa_command("initiate:immediate", wait_until_done=True)
+
+    def initialize_trace_transfer(self):
+        self.visa_instr.write("format:data real,64")
+        self.visa_instr.write("format:border norm")
+
+    def start_trace(self):
+        self.run_mode = self.RunModes.SINGLE
+        self.start_measurement()
+
+    def acquire_trace(self, trace_num=1, collect_metadata=True):
+        self.initialize_trace_transfer()
+        self.start_trace()
+        self.wait_until_done()
+        raw_data = self.visa_instr.query_binary_values(f"trace:data? trace{trace_num}", datatype="d", is_big_endian=True, container=np.array)
+
+        if collect_metadata:
+            metadata = self.collect_metadata()
+        else:
+            metadata = {}
+
+        if self.span == 0.0:
+            # we're zero-spanning, x-axis is time axis
+            x_axis = np.linspace(0.0, self.sweep_time, num=self.record_length)
+            metadata["x_unit"] = 's'
+            metadata['x_label'] = 'time'
+        else:
+            x_axis = np.linspace(self.start_frequency, self.stop_frequency, num=self.record_length)
+            metadata["x_unit"] = 'Hz'
+            metadata["x_label"] = "frequency"
+
+        metadata["y_label"] = 'signal'
+
+        data_obj = pylabframe.data.NumericalData(raw_data, x_axis=x_axis, metadata=metadata)
+        return data_obj
+
+        return raw_data
