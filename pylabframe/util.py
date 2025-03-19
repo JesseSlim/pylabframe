e8313f0912e8ffae115c17829dd7db001437cce0
Experiment <Experiment@SMP-4FQTKJ3.uq.edu.au>
2025-03-19 11:22:24 +1000

    syntax warning fix

--- pylabframe/util.py
+++ pylabframe/util.py
@@ -9,10 +9,10 @@ def map_nested_dict(f, d):
         return f(d)
 
 
-def extract_tag_value(filename, tag, conv=lambda s: float(s.replace("_",".")), value_re="[0-9]+((_|\.)[0-9]+)?", raise_on_absence=False, split_filename=True):
+def extract_tag_value(filename, tag, conv=lambda s: float(s.replace("_",".")), value_re="[0-9]+((_|\\.)[0-9]+)?", raise_on_absence=False, split_filename=True):
     if split_filename:
         filename = os.path.split(filename)[1]
-    m = re.search(f"(_|^){tag}(?P<value>{value_re})(_|\.|$)", filename)
+    m = re.search(f"(_|^){tag}(?P<value>{value_re})(_|\\.|$)", filename)
     if m is None:
         if raise_on_absence:
             raise ValueError(tag)
